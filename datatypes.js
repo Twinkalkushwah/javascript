561c75ec002dc2e8183f2f494e83296d4e488f77
Twinkal Kushwah <108083295+Twinkalkushwah@users.noreply.github.com>
2024-03-28 15:09:26 +0530

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -1,12 +1,1 @@
-"use strict"; // treat all js code as newer version
-//data types
-//number
-//BigInt
-//String
-//boolean
-//null--> standalone value
-//undefined
-//symbol-->unique
 
-console.log(typeof null); //object
-console.log(typeof undefined);//undefined
